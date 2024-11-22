78ca924e050a65b3e7dce8b2a238ae7c41c41c3f
A H M E D <70113157+ahmed-03@users.noreply.github.com>
2024-11-22 20:31:32 +0300

    Update Register.sv

--- Register/Register/Register.srcs/sources_1/new/Register.sv
+++ Register/Register/Register.srcs/sources_1/new/Register.sv
@@ -16,7 +16,7 @@ module Register #(
     output logic [bits-1:0] q;
 
   always @(posedge clk, negedge rst_n) begin
-    if (~rst_n) q <= 0;
+    if (~rst_n) q <= '0;
     else if (en) q <= d;
     else q <= q;
   end
